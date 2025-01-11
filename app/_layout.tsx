42504073fa2a86411a7893b1b0fd3f604e2722c1
Khang Nguyen <khang.nguyen@htgsoft.com>
2025-01-11 15:22:58 +0700

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { View,Text,StyleSheet } from 'react-native';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,31 +19,39 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   //CSS is here
   const styles = StyleSheet.create({
-    container:{
+    container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems:'center',
-      justifyContent:'center'
+      backgroundColor: "#fff",
+      // alignItems: "center",
+      // justifyContent: "center",
+      paddingHorizontal: 4,
+    },
+    header: {
+     
+      textAlign: "center",
+      fontSize: 30,
+      backgroundColor: "#ffff00",
+    },
+    text_input : {
+      borderWidth : 1,
+      borderRadius: 5,
+      borderColor: "#0066ff",
+      height: 40,
     }
-  })
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <View style={styles.container}>
-      <Text>Hello is here</Text> 
+      <View style={styles.header}>Todo APP</View>
+
+      <View>
+        <Text >Input data </Text>
+        <TextInput style={styles.text_input} />
+      </View>
+
+      <View>
+        <Text>List </Text>
+      </View>
     </View>
   );
 }
