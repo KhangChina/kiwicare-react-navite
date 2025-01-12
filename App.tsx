6a9a500a4977abde8fae22e29d5f295c46260533
Khang Nguyen <khang.nguyen@htgsoft.com>
2025-01-12 23:26:27 +0700

    update

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Onboarding } from './components/Onboarding';
+
+export default function App() {
+  return (
+    <SafeAreaView  style={{ flex: 1 }}>
+      <Onboarding />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/Onboarding.tsx
@@ -0,0 +1,188 @@
+import React, { useRef, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  useWindowDimensions,
+  Image,
+  Animated,
+  Button,
+  TouchableOpacity,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from "react-native";
+const slides = [
+  {
+    id: "1",
+    image: require("../assets/slider/1.png"),
+    title: "Create a prototype in just a few minutes 1",
+    description:
+      "Enjoy these pre-made components and worry only about creating the best product ever.",
+  },
+  {
+    id: "2",
+    image: require("../assets/slider/1.png"),
+    title: "Create a prototype in just a few minutes 2",
+    description:
+      "Enjoy these pre-made components and worry only about creating the best product ever.",
+  },
+  {
+    id: "3",
+    image: require("../assets/slider/1.png"),
+    title: "Create a prototype in just a few minutes 3",
+    description:
+      "Enjoy these pre-made components and worry only about creating the best product ever.",
+  },
+];
+const count = slides.length;
+export function Onboarding() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const flatListRef = useRef<FlatList>(null);
+  const [textButton, setTextButton] = useState("Next");
+
+  const handleNext = () => {
+    if (currentIndex < count - 1) {
+      const nextIndex = currentIndex + 1;
+      flatListRef.current?.scrollToIndex({ index: nextIndex });
+      setCurrentIndex(nextIndex); 
+      if (nextIndex == count - 1) {
+        setTextButton("Login");
+      } else {
+        setTextButton("Next");
+      }
+    }
+  };
+  const { width } = useWindowDimensions();
+  const handleScrollEnd = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const offsetX = event.nativeEvent.contentOffset.x;
+    const index = Math.round(offsetX / width); 
+    setCurrentIndex(index);
+
+    if (index == count - 1) {
+      setTextButton("Login");
+    } else {
+      setTextButton("Next");
+    }
+
+  };
+
+  return (
+    <View style={styles.onboarding}>
+      <FlatList
+        ref={flatListRef}
+        data={slides}
+        renderItem={({ item }) => <OnboardingItem item={item} count={count} />}
+        keyExtractor={(item) => item.id}
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={handleScrollEnd}
+      />
+      <View style={styles.submit}>
+        <TouchableOpacity style={styles.button} onPress={handleNext}>
+          <Text style={styles.text_button}>{textButton}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+export function OnboardingItem({ item, count }: { item: any; count: any }) {
+  function nextScreen() {}
+
+  const { width, height } = useWindowDimensions();
+  return (
+    <View style={[styles.item, { width: width }]}>
+      <Image
+        source={item.image}
+        style={[styles.image, { width: width, height: height * 0.6 }]}
+        resizeMode="cover"
+      />
+      <View style={[styles.text_banner, { width: width }]}>
+        <Paginator data={item} count={count} />
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+      </View>
+    </View>
+  );
+}
+
+export function Paginator({ data, count }: { data: any; count: any }) {
+  return (
+    <View style={styles.paginator}>
+      {Array.from({ length: count }).map((_, index) => (
+        <View
+          key={index}
+          style={[
+            styles.dot,
+            index === data.id - 1 && styles.activeDot, // Kiểm tra và áp dụng style active
+          ]}
+        />
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  onboarding: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  item: {
+    flexDirection: "column",
+    flex: 1,
+  },
+  image: {},
+  text_banner: {
+    flex: 1,
+    flexDirection: "column",
+    paddingHorizontal: 24,
+    paddingTop: 24,
+  },
+  paginator: {
+    paddingTop: 16,
+    flexDirection: "row",
+    flex: 1,
+    height: 24,
+    paddingBottom: 24,
+  },
+  dot: {
+    height: 8,
+    width: 8,
+    borderRadius: 24,
+    backgroundColor: "#8F9098",
+    marginRight: 8,
+  },
+  activeDot: {
+    backgroundColor: "#006FFD",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  description: {
+    paddingTop: 24,
+    color: "#71727A",
+    fontSize: 12,
+    width: 290,
+  },
+  submit: {
+    paddingHorizontal: 24,
+    paddingTop: 32,
+    paddingBottom: 24,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 12,
+    height: 48,
+    backgroundColor: "#006FFD",
+  },
+  text_button: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+});
