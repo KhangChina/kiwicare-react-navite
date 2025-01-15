258b5604600245b433193c2d26668c0d5af09a96
Khang Nguyen <khang.nguyen@htgsoft.com>
2025-01-15 10:48:55 +0700

    update

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,9 +5,7 @@ import {
   StaticParamList,
 } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Image, View } from "react-native";
-import bell from "../assets/bell.png";
-import newspaper from "../assets/newspaper.png";
+import { Image, TouchableOpacity, View } from "react-native";
 import { Home } from "./screens/Home";
 import { Settings } from "./screens/Settings";
 import { Updates } from "./screens/Updates";
@@ -16,68 +14,156 @@ import Login from "./screens/Login";
 import Onboarding from "./screens/Onboarding";
 import { HeaderRight } from "./headers/header-right";
 import { HeaderLeft } from "./headers/header-left";
-import { Ionicons } from "@expo/vector-icons";
 
-const actionTab = (focused: any) => {
-  console.log(focused);
-  if (focused) {
-    return (
+const render_main = (iconSource: any, labelText: any, textColor: any) => {
+  return (
+    <View
+      style={{
+        // flexDirection: "row",
+        width: 82,
+        // height:40,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Image
-        source={require("../assets/explore_active.png")}
-        // tintColor="#006FFD"
+        source={iconSource}
         style={{
           width: 24,
           height: 24,
         }}
       />
-    );
-  }
+      <Text
+        style={{
+          fontSize: 16,
+          color: textColor,
+          fontWeight: 300,
+        }}
+      >
+        {labelText}
+      </Text>
+    </View>
+  );
+};
+
+const render_contact_main = () => {
   return (
-    <Image
-      source={require("../assets/Explore.png")}
-      //tintColor="#D4D6DD"
+    <View
       style={{
-        width: 24,
-        height: 24,
+        position: "absolute",
+        bottom: 30,
+        width: 80,
+        height: 80,
+        borderRadius: 100, // Giữ hình tròn
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+        shadowColor: "#000", // Màu bóng (đen)
+        shadowOffset: { width: 0, height: 2 }, // Độ lệch của bóng
+        shadowOpacity: 0.3, // Độ mờ của bóng
+        shadowRadius: 5, // Độ lan tỏa của bóng
+        elevation: 5, // Thêm hiệu ứng bóng cho Android
       }}
-    />
+    >
+      <Image
+        source={require("../assets/service.png")}
+        style={{
+          width: 50,
+          height: 50,
+        }}
+      />
+    </View>
   );
 };
 
 const HomeTabs = createBottomTabNavigator({
+  screenOptions: ({ route }) => ({
+    tabBarIcon: ({ focused }) => {
+      let iconName;
+      let labelText;
+      let textColor;
+      switch (route.name) {
+        case "Home":
+          labelText = "Home";
+          iconName = focused
+            ? require("../assets/explore_active.png")
+            : require("../assets/Explore.png");
+          textColor = focused ? "#1F2024" : "#71727A";
+          break;
+        case "Categories":
+          iconName = focused
+            ? require("../assets/categories_active.png")
+            : require("../assets/categories.png");
+          labelText = "Categories";
+          textColor = focused ? "#1F2024" : "#71727A";
+          break;
+        case "Stores":
+          iconName = focused
+            ? require("../assets/stores_active.png")
+            : require("../assets/stores.png");
+          labelText = "Stores";
+          textColor = focused ? "#1F2024" : "#71727A";
+          break;
+        case "Profile":
+          iconName = focused
+            ? require("../assets/profiles_active.png")
+            : require("../assets/profile.png");
+          labelText = "Profile";
+          textColor = focused ? "#1F2024" : "#71727A";
+          break;
+        case "Special":
+          return render_contact_main();
+        default:
+          iconName = require("../assets/newspaper.png");
+          labelText = "Menu";
+          textColor = "#71727A";
+      }
+      return render_main(iconName, labelText, textColor);
+    },
+    tabBarStyle: {
+      height: 88,
+      justifyContent: "center",
+      alignItems: "center",
+      paddingTop: 30,
+    },
+    tabBarButton: (props) => (
+      <TouchableOpacity {...props} activeOpacity={0.6}>
+        {props.children}
+      </TouchableOpacity>
+    ),
+    tabBarShowLabel: false,
+  }),
+
   screens: {
     Home: {
       screen: Home,
       options: {
-        // title: "Feed",
         headerShown: false,
-        tabBarIcon: ({ focused }) => actionTab(focused),
-        tabBarLabelStyle: {
-          fontSize: 16,
-          fontFamily: "Georgia",
-          fontWeight: 300,
-        },
-        tabBarStyle:{
-          height:88,
-          justifyContent: 'center',
-          alignItems:'center',
-          paddingTop:16,
-         },
       },
     },
-    Updates: {
+    Categories: {
       screen: Updates,
       options: {
-        tabBarIcon: ({ color, size }) => (
-          <Image
-            source={bell}
-            tintColor={color}
-            style={{
-              width: size,
-              height: size,
-            }}
-          />
-        ),
+        headerShown: false,
+      },
+    },
+    Special: {
+      screen: Updates,
+      headerShown: false,
+      tabBarStyle: {
+        backgroundColor: "transparent",
+      },
+    },
+    Stores: {
+      screen: Updates,
+      options: {
+        headerShown: false,
+      },
+    },
+    Profile: {
+      screen: Updates,
+      options: {
+        headerShown: false,
       },
     },
   },
