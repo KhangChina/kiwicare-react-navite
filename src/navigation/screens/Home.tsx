ae0571e60f3340b4f43d4a8c8f496d25d766c845
Khang Nguyen <khang.nguyen@htgsoft.com>
2025-01-15 16:55:46 +0700

    updater

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -8,42 +8,139 @@ import {
   Button,
   ViewStyle,
   useWindowDimensions,
+  FlatList,
+  ScrollView,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SliderBox } from "react-native-image-slider-box";
 export function Home() {
   const { width } = useWindowDimensions();
-  const views1 = [require("../../assets/1.png"), require("../../assets/1.png")];
+  const images_banner = [
+    require("../../assets/1.png"),
+    require("../../assets/1.png"),
+    require("../../assets/1.png"),
+    require("../../assets/1.png"),
+    require("../../assets/1.png"),
+  ];
 
   const navigation = useNavigation();
+
+
+    const DATA = [
+      {
+        id: 1,
+        title: 'First Item',
+      },
+      {
+        id: 2,
+        title: 'Second Item',
+      },
+      {
+        id: 3,
+        title: 'Third Item',
+      },
+    ];
+    const Item = ({title}: any) => (
+      <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    );
+    
+
+
+
   function logout() {
     navigation.navigate("Login");
   }
   return (
-    <View style={styles.container}>
-      <View  style={[styles.slider_box, {width: width}]}>
+    <ScrollView  style={styles.container}>
+      <View style={{ width: width }}>
         <SliderBox
-          images={views1}
+          images={images_banner}
           ImageComponentStyle={{
             resizeMode: "cover",
           }}
         />
       </View>
-      <Text>Home Screen</Text>
-      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
-      <Button title="logout" onPress={logout} />
-    </View>
+
+      <View style={styles.container_perfect}>
+        <View style={styles.container_text}>
+          <Text style={styles.perfect_text}>Perfect for you</Text>
+
+          <TouchableOpacity>
+            <Text style={styles.see_more}>See more</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+        data={DATA}
+        renderItem={({item}: any) => <Item title={item.title} />}
+        horizontal={true} 
+        showsHorizontalScrollIndicator = {false}
+        contentContainerStyle={{ paddingHorizontal: 16 }} 
+        keyExtractor={(item: any) => item.id}
+      />
+      </View>
+
+      <View style={styles.container_summer}>
+        <View style={styles.container_text}>
+          <Text style={styles.perfect_text}>Perfect for you</Text>
+
+          <TouchableOpacity>
+            <Text style={styles.see_more}>See more</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+        data={DATA}
+        renderItem={({item}: any) => <Item title={item.title} />}
+        horizontal={true} 
+        showsHorizontalScrollIndicator = {false}
+        contentContainerStyle={{ paddingHorizontal: 16 }} 
+        keyExtractor={(item: any) => item.id}
+      />
+      </View>
+
+    </ScrollView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    //flex: 1,
+    flex: 1,
     flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor:"#F8F9FE",
   },
-  slider_box: {
-    height: 241,
+  container_perfect: {
+    paddingTop:24,
+  },
+  container_summer :{
+    paddingTop:40,
+  },
+  container_text: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal:24
+  },
+  perfect_text : {
+    fontSize: 14,
+    fontWeight:700,
+    color: "#000000"
+  },
+  see_more: {
+    fontSize: 12,
+    fontWeight:600,
+    color: "#006FFD"
+  },
+  item: {
+    width: 200,
+    backgroundColor: '#EAF2FF',
+    marginRight: 12,
+    height:189,
+    borderRadius:16
+  },
+  title: {
+    fontSize: 32,
   },
 });
