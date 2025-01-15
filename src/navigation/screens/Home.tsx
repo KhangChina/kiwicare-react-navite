c33d8a6246f44cad4bcdfcd59a29b5a42883cb8f
Khang Nguyen <khang.nguyen@htgsoft.com>
2025-01-15 17:01:41 +0700

    update

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -25,35 +25,31 @@ export function Home() {
 
   const navigation = useNavigation();
 
-
-    const DATA = [
-      {
-        id: 1,
-        title: 'First Item',
-      },
-      {
-        id: 2,
-        title: 'Second Item',
-      },
-      {
-        id: 3,
-        title: 'Third Item',
-      },
-    ];
-    const Item = ({title}: any) => (
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
-    );
-    
-
-
+  const DATA = [
+    {
+      id: 1,
+      title: "First Item",
+    },
+    {
+      id: 2,
+      title: "Second Item",
+    },
+    {
+      id: 3,
+      title: "Third Item",
+    },
+  ];
+  const Item = ({ title }: any) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
 
   function logout() {
     navigation.navigate("Login");
   }
   return (
-    <ScrollView  style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={{ width: width }}>
         <SliderBox
           images={images_banner}
@@ -73,13 +69,13 @@ export function Home() {
         </View>
 
         <FlatList
-        data={DATA}
-        renderItem={({item}: any) => <Item title={item.title} />}
-        horizontal={true} 
-        showsHorizontalScrollIndicator = {false}
-        contentContainerStyle={{ paddingHorizontal: 16 }} 
-        keyExtractor={(item: any) => item.id}
-      />
+          data={DATA}
+          renderItem={({ item }: any) => <Item title={item.title} />}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 16 }}
+          keyExtractor={(item: any) => item.id}
+        />
       </View>
 
       <View style={styles.container_summer}>
@@ -92,16 +88,36 @@ export function Home() {
         </View>
 
         <FlatList
-        data={DATA}
-        renderItem={({item}: any) => <Item title={item.title} />}
-        horizontal={true} 
-        showsHorizontalScrollIndicator = {false}
-        contentContainerStyle={{ paddingHorizontal: 16 }} 
-        keyExtractor={(item: any) => item.id}
-      />
+          data={DATA}
+          renderItem={({ item }: any) => <Item title={item.title} />}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 16 }}
+          keyExtractor={(item: any) => item.id}
+        />
       </View>
 
-    </ScrollView >
+      {/* <View style={styles.container_summer}>
+        <View style={styles.container_text}>
+          <Text style={styles.perfect_text}>Your events</Text>
+
+          <TouchableOpacity>
+            <Text style={styles.see_more}>See more</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={DATA}
+          renderItem={({ item }: any) => <Item title={item.title} />}
+          keyExtractor={(item: any) => item.id}
+        />
+      </View> */}
+
+
+
+
+
+    </ScrollView>
   );
 }
 
@@ -109,36 +125,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    backgroundColor:"#F8F9FE",
+    backgroundColor: "#F8F9FE",
   },
   container_perfect: {
-    paddingTop:24,
+    paddingTop: 24,
   },
-  container_summer :{
-    paddingTop:40,
+  container_summer: {
+    paddingTop: 40,
   },
   container_text: {
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
-    paddingHorizontal:24
+    paddingHorizontal: 24,
   },
-  perfect_text : {
+  perfect_text: {
     fontSize: 14,
-    fontWeight:700,
-    color: "#000000"
+    fontWeight: 700,
+    color: "#000000",
   },
   see_more: {
     fontSize: 12,
-    fontWeight:600,
-    color: "#006FFD"
+    fontWeight: 600,
+    color: "#006FFD",
   },
   item: {
     width: 200,
-    backgroundColor: '#EAF2FF',
+    backgroundColor: "#EAF2FF",
     marginRight: 12,
-    height:189,
-    borderRadius:16
+    height: 189,
+    borderRadius: 16,
   },
   title: {
     fontSize: 32,
